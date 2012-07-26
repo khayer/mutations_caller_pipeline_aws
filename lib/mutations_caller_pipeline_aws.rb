c4b522341269d533400c41228c9f67f1af99fd47
Katharina Hayer <katharinaehayer@gmail.com>
2012-07-26 15:47:26 -0400

    Added picard tools

--- lib/mutations_caller_pipeline_aws.rb
+++ lib/mutations_caller_pipeline_aws.rb
@@ -1,6 +1,7 @@
 require 'mutations_caller_pipeline_aws/bwa_caller'
 require 'mutations_caller_pipeline_aws/gatk_caller'
 require 'mutations_caller_pipeline_aws/samtools_indexing'
+require 'mutations_caller_pipeline_aws/picard_caller'
 
 class MutationsCallerPipelineAws
   def self.hi
--- lib/mutations_caller_pipeline_aws/bwa_caller.rb
+++ lib/mutations_caller_pipeline_aws/bwa_caller.rb
@@ -1,11 +1,10 @@
 class BwaCaller
 
-  def self.call_paired_end(r1, r2, sai1, sai2,  out_file, index, log_file, bwa, samtools, job_prefix,account, debug)
+  def self.call_paired_end(r1, r2, sai1, sai2,  bam_file, index, log_file, bwa, samtools, job_prefix,account, debug)
     dummy = "\\\\\\"
     cmd = "qsub -o #{log_file} -hold_jid bwa_aln_#{job_prefix} -V -cwd -b y -N bwa_#{job_prefix} -l h_vmem=6G -pe make 3  #{account}\
-           #{bwa} sampe -r '@RG#{dummy}tID:foo#{dummy}tSM:bar#{dummy}tPL:Illumina' #{index} \
-          #{sai1} #{sai2}  #{r1} #{r2} \
-          \\| #{samtools} view -Su - \\| #{samtools} sort - #{out_file}"
+           #{bwa} sampe  \
+          #{sai1} #{sai2}  #{r1} #{r2} -f #{bam_file}"
     puts cmd
     system('bash','-c', cmd) if debug == 1
   end
