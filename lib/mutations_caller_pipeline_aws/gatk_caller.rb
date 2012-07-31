c9c89b2a85c1617b7588b4fe13da3873cc23f4e0
Katharina Hayer <katharinaehayer@gmail.com>
2012-07-31 09:47:42 -0400

    errors should be in one log file

--- lib/mutations_caller_pipeline_aws/gatk_caller.rb
+++ lib/mutations_caller_pipeline_aws/gatk_caller.rb
@@ -2,7 +2,7 @@ class GatkCaller
   # INDEX is normal genom.fa
   # Genotyper
   def self.call(log_dir, gatk, index_fa, read_bam, read_vcf, job_prefix, account,dbsnp_file, debug)
-    cmd = "qsub -o #{log_dir} -V -cwd -b y -N genotyper_#{job_prefix} -l h_vmem=9G -hold_jid recalibration_#{job_prefix} #{account}\
+    cmd = "qsub -o #{log_dir} -e errors_#{log_dir} -V -cwd -b y -N genotyper_#{job_prefix} -l h_vmem=9G -hold_jid recalibration_#{job_prefix} #{account}\
       #{gatk} -l INFO -R #{index_fa} -T UnifiedGenotyper \
       -I #{read_bam} --dbsnp #{dbsnp_file} \
       -o #{read_vcf} \
@@ -14,7 +14,7 @@ class GatkCaller
   # INDEX is normal genom.fa
   # Coverage Summary
   def self.coverage(log_dir, gatk, index_fa, read_bam, outfile_prefix, job_prefix, account, debug)
-    cmd = "qsub -o #{log_dir} -V -cwd -b y -N genotyper_#{job_prefix} -l h_vmem=9G -hold_jid recalibration_#{job_prefix} #{account}\
+    cmd = "qsub -o #{log_dir} -e errors_#{log_dir} -V -cwd -b y -N genotyper_#{job_prefix} -l h_vmem=9G -hold_jid recalibration_#{job_prefix} #{account}\
       #{gatk} -R #{index_fa} -T DepthOfCoverage \
       -I #{read_bam} --omitDepthOutputAtEachBase \
       -o #{outfile_prefix} --omitIntervalStatistics --omitLocusTable"
@@ -24,7 +24,7 @@ class GatkCaller
 
   # Making recalibration table
   def self.recalibrate_bam(log_dir ,gatk, index_fa, read_bam, recal_file, job_prefix, account, dbsnp_file, debug )
-    cmd = "qsub -o #{log_dir} -V -cwd -b y -N recalibration_table_#{job_prefix} -l h_vmem=9G  -hold_jid realignment_#{job_prefix} #{account} \
+    cmd = "qsub -o #{log_dir} -e errors_#{log_dir} -V -cwd -b y -N recalibration_table_#{job_prefix} -l h_vmem=9G  -hold_jid realignment_#{job_prefix} #{account} \
       #{gatk} -knownSites #{dbsnp_file} -I #{read_bam} \
       -R #{index_fa} -T CountCovariates \
       -cov ReadGroupCovariate -cov QualityScoreCovariate -cov DinucCovariate \
@@ -36,7 +36,7 @@ class GatkCaller
 
   # Using recalibration table
   def self.table_calibration(log_dir, gatk, index_fa, read_bam, recal_bam, recal_file, job_prefix, account, debug)
-    cmd = "qsub -V -o #{log_dir} -cwd -b y -N recalibration_#{job_prefix} -l h_vmem=9G -hold_jid recalibration_table_#{job_prefix} #{account} \
+    cmd = "qsub -V -o #{log_dir} -e errors_#{log_dir} -cwd -b y -N recalibration_#{job_prefix} -l h_vmem=9G -hold_jid recalibration_table_#{job_prefix} #{account} \
       #{gatk} \
       -R #{index_fa} \
       -I #{read_bam} \
@@ -50,7 +50,7 @@ class GatkCaller
   # Preparation realignement
 
   def self.prepare_realigne(log_dir, gatk, read_bam, index_fa, target_intervals, job_prefix, account, dbsnp_file, debug)
-    cmd = "qsub -o #{log_dir} -V -cwd -b y -N prep_realignment_#{job_prefix} -l h_vmem=9G -hold_jid index_#{job_prefix} #{account}\
+    cmd = "qsub -o #{log_dir} -e errors_#{log_dir} -V -cwd -b y -N prep_realignment_#{job_prefix} -l h_vmem=9G -hold_jid index_#{job_prefix} #{account}\
       #{gatk} \
       -I #{read_bam} --known #{dbsnp_file} \
       -R #{index_fa} \
@@ -62,7 +62,7 @@ class GatkCaller
 
   # Realignment
   def self.realigne(log_dir, gatk, read_bam, index_fa, target_intervals, realigned_bam, job_prefix, account, debug)
-    cmd = "qsub -o #{log_dir} -V -cwd -b y -N realignment_#{job_prefix} -l h_vmem=9G -hold_jid prep_realignment_#{job_prefix} #{account} \
+    cmd = "qsub -o #{log_dir} -e errors_#{log_dir} -V -cwd -b y -N realignment_#{job_prefix} -l h_vmem=9G -hold_jid prep_realignment_#{job_prefix} #{account} \
       #{gatk} \
       -I #{read_bam} \
       -R #{index_fa} \
--- lib/mutations_caller_pipeline_aws/picard_caller.rb
+++ lib/mutations_caller_pipeline_aws/picard_caller.rb
@@ -1,13 +1,13 @@
 class PicardCaller
   #converter = "java -jar ~/Downloads/picard-tools-1.56/picard-tools-1.56/SamFormatConverter.jar I=WT_aligned_sorted_rg.bam O=tmp.sam VALIDATION_STRINGENCY=LENIENT"
   def self.convert(sam_file, bam_file, picard_tools, log_file, job_prefix, account)
-    cmd = "qsub -o #{log_file} -V -cwd -b y -hold_jid bwa_#{job_prefix} -N convert_#{job_prefix} -l h_vmem=2G #{account} \
+    cmd = "qsub -o #{log_file} -e errors_#{log_dir} -V -cwd -b y -hold_jid bwa_#{job_prefix} -N convert_#{job_prefix} -l h_vmem=2G #{account} \
       java -jar #{picard_tools}/SamFormatConverter.jar I=#{sam_file} O=#{bam_file} VALIDATION_STRINGENCY=LENIENT"
   end
 
   #rg_and_sorting = "java -jar -Xmx3g ~/Downloads/picard-tools-1.56/picard-tools-1.56/AddOrReplaceReadGroups.jar I=WT_aligned.bam O=WT_aligned_sorted_rg.bam SO=coordinate ID=15 LB=nina_library PL=Illumina PU=ATCATC SM=My_test VALIDATION_STRINGENCY=LENIENT"
   def self.rg_and_sorting(bam_file, bam_file_sorted, picard_tools, library, index, sample_name, log_file, id, job_prefix, account)
-    cmd = "qsub -o #{log_file} -V -cwd -b y -hold_jid convert_#{job_prefix} -N sort_#{job_prefix} -l h_vmem=4G #{account} \
+    cmd = "qsub -o #{log_file} -e errors_#{log_dir} -V -cwd -b y -hold_jid convert_#{job_prefix} -N sort_#{job_prefix} -l h_vmem=4G #{account} \
       java -jar -Xmx3g #{picard_tools}/AddOrReplaceReadGroups.jar I=#{bam_file} O=#{bam_file_sorted} SO=coordinate ID=#{id} \
       LB=#{library} PL=Illumina PU=#{index} SM=#{sample_name} VALIDATION_STRINGENCY=LENIENT"
   end
@@ -15,14 +15,14 @@ class PicardCaller
 
   #mark_dublicates = "java -jar ~/Downloads/picard-tools-1.56/picard-tools-1.56/MarkDuplicates.jar I=WT_aligned_sorted_rg.bam O=marked_dublicates.bam M=dublicate.metrcis AS=true VALIDATION_STRINGENCY=LENIENT"
   def self.mark_dublicates(bam_file_sorted, bam_file_sorted_dublicates, dublicate_metrcis, picard_tools, log_file, job_prefix, account)
-    cmd = "qsub -o #{log_file} -V -cwd -b y -hold_jid sort_#{job_prefix} -N dublicates_#{job_prefix} -l h_vmem=2G #{account} \
+    cmd = "qsub -o #{log_file} -e errors_#{log_dir} -V -cwd -b y -hold_jid sort_#{job_prefix} -N dublicates_#{job_prefix} -l h_vmem=2G #{account} \
       java -jar #{picard_tools}/MarkDuplicates.jar I=#{bam_file_sorted} O=#{bam_file_sorted_dublicates} M=#{dublicate_metrcis} \
       AS=true VALIDATION_STRINGENCY=LENIENT"
   end
 
   #build_index = "java -jar ~/Downloads/picard-tools-1.56/picard-tools-1.56/BuildBamIndex.jar I=marked_dublicates.bam  VALIDATION_STRINGENCY=LENIENT"
   def self.build_index(bam_file_sorted_dublicates, picard_tools, log_file, job_prefix, account)
-    cmd = "qsub -o #{log_file} -V -cwd -b y -hold_jid dublicates_#{job_prefix} -N index_#{job_prefix} -l h_vmem=2G #{account} \
+    cmd = "qsub -o #{log_file} -e errors_#{log_dir} -V -cwd -b y -hold_jid dublicates_#{job_prefix} -N index_#{job_prefix} -l h_vmem=2G #{account} \
       java -jar #{picard_tools}/BuildBamIndex.jar I=#{bam_file_sorted_dublicates}  VALIDATION_STRINGENCY=LENIENT"
   end
 end
