31874c108106d106816d1876e15a32460bc285dc
Katharina Hayer <katharinaehayer@gmail.com>
2012-07-31 09:49:25 -0400

    fixed bug

--- lib/mutations_caller_pipeline_aws/bwa_caller.rb
+++ lib/mutations_caller_pipeline_aws/bwa_caller.rb
@@ -2,7 +2,7 @@ class BwaCaller
 
   def self.call_paired_end(r1, r2, sai1, sai2,  sam_file, index, log_file, bwa, samtools, job_prefix,account, debug)
     dummy = "\\\\\\"
-    cmd = "qsub -o #{log_file} -e errors_#{log_dir} -hold_jid bwa_aln_#{job_prefix} -V -cwd -b y -N bwa_#{job_prefix} -l h_vmem=6G -pe make 3  #{account}\
+    cmd = "qsub -o #{log_file} -e errors_#{log_file} -hold_jid bwa_aln_#{job_prefix} -V -cwd -b y -N bwa_#{job_prefix} -l h_vmem=6G -pe make 3  #{account}\
            #{bwa} sampe  #{index}   \
           #{sai1} #{sai2}  #{r1} #{r2} -f #{sam_file}"
     puts cmd
@@ -10,7 +10,7 @@ class BwaCaller
   end
 
   def self.call_aln(read, index, out_file, log_file, bwa, job_prefix, account,debug)
-    cmd = "qsub -o #{log_file} -e errors_#{log_dir} -V -cwd -b y -N bwa_aln_#{job_prefix} -l h_vmem=4G #{account} \
+    cmd = "qsub -o #{log_file} -e errors_#{log_file} -V -cwd -b y -N bwa_aln_#{job_prefix} -l h_vmem=4G #{account} \
            #{bwa} aln -f #{out_file} #{index} #{read} "
     puts cmd
     system(cmd) if debug == 1
