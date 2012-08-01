c2405d10c5dab384faee1a312672b4e2a3d01d1d
Katharina Hayer <katharinaehayer@gmail.com>
2012-08-01 14:19:40 -0400

    Seperated error files again, otherwise no way of telling what went wrong

--- lib/mutations_caller_pipeline_aws/gatk_caller.rb
+++ lib/mutations_caller_pipeline_aws/gatk_caller.rb
@@ -2,7 +2,7 @@ class GatkCaller
   # INDEX is normal genom.fa
   # Genotyper
   def self.call(log_dir, gatk, index_fa, read_bam, read_vcf, job_prefix, account,dbsnp_file, debug)
-    cmd = "qsub -pe DJ 4 -o #{log_dir} -e errors_#{log_dir} -V -cwd -b y -N genotyper_#{job_prefix} -l h_vmem=6G -hold_jid recalibration_#{job_prefix} #{account}\
+    cmd = "qsub -pe DJ 4 -o #{log_dir} -e genotyper_errors_#{log_dir} -V -cwd -b y -N genotyper_#{job_prefix} -l h_vmem=6G -hold_jid recalibration_#{job_prefix} #{account}\
       java -Xmx16g -jar #{gatk} -l INFO -R #{index_fa} -T UnifiedGenotyper \
       -I #{read_bam} --dbsnp #{dbsnp_file} \
       -o #{read_vcf} -nt 4 \
@@ -15,7 +15,7 @@ class GatkCaller
   # Coverage Summary
   # parallel not possible yet (1.6-13-g91f02df)
   def self.coverage(log_dir, gatk, index_fa, read_bam, outfile_prefix, job_prefix, account, debug)
-    cmd = "qsub -o #{log_dir} -e errors_#{log_dir} -V -cwd -b y -N coverage_#{job_prefix} -l h_vmem=11G -hold_jid recalibration_#{job_prefix} #{account}\
+    cmd = "qsub -o #{log_dir} -e coverage_errors_#{log_dir} -V -cwd -b y -N coverage_#{job_prefix} -l h_vmem=11G -hold_jid recalibration_#{job_prefix} #{account}\
       java -Xmx10g -jar #{gatk} -R #{index_fa} -T DepthOfCoverage \
       -I #{read_bam} --omitDepthOutputAtEachBase \
       -o #{outfile_prefix} --omitIntervalStatistics --omitLocusTable"
@@ -25,7 +25,7 @@ class GatkCaller
 
   # Making recalibration table
   def self.recalibrate_bam(log_dir ,gatk, index_fa, read_bam, recal_file, job_prefix, account, dbsnp_file, debug )
-    cmd = "qsub -pe DJ 4 -o #{log_dir} -e errors_#{log_dir} -V -cwd -b y -N recalibration_table_#{job_prefix} -l h_vmem=6G  -hold_jid realignment_#{job_prefix} #{account} \
+    cmd = "qsub -pe DJ 4 -o #{log_dir} -e recalibrate_errors_#{log_dir} -V -cwd -b y -N recalibration_table_#{job_prefix} -l h_vmem=6G  -hold_jid realignment_#{job_prefix} #{account} \
       java -Xmx16g -jar #{gatk} -knownSites #{dbsnp_file} -I #{read_bam} \
       -R #{index_fa} -T CountCovariates -nt 4 \
       -cov ReadGroupCovariate -cov QualityScoreCovariate -cov DinucCovariate \
@@ -38,7 +38,7 @@ class GatkCaller
   # Using recalibration table
   # parallel not possible yet (1.6-13-g91f02df)
   def self.table_calibration(log_dir, gatk, index_fa, read_bam, recal_bam, recal_file, job_prefix, account, debug)
-    cmd = "qsub -V -o #{log_dir} -e errors_#{log_dir} -cwd -b y -N recalibration_#{job_prefix} -l h_vmem=11G -hold_jid recalibration_table_#{job_prefix} #{account} \
+    cmd = "qsub -V -o #{log_dir} -e prep_recal_errors_#{log_dir} -cwd -b y -N recalibration_#{job_prefix} -l h_vmem=11G -hold_jid recalibration_table_#{job_prefix} #{account} \
       java -Xmx10g -jar #{gatk} \
       -R #{index_fa} \
       -I #{read_bam} \
@@ -52,7 +52,7 @@ class GatkCaller
   # Preparation realignement
 
   def self.prepare_realigne(log_dir, gatk, read_bam, index_fa, target_intervals, job_prefix, account, dbsnp_file, debug)
-    cmd = "qsub -pe DJ 4 -o #{log_dir} -e errors_#{log_dir} -V -cwd -b y -N prep_realignment_#{job_prefix} -l h_vmem=6G -hold_jid index_#{job_prefix} #{account}\
+    cmd = "qsub -pe DJ 4 -o #{log_dir} -e prep_realign_errors_#{log_dir} -V -cwd -b y -N prep_realignment_#{job_prefix} -l h_vmem=6G -hold_jid index_#{job_prefix} #{account}\
       java -Xmx16g -jar #{gatk} -nt 4 \
       -I #{read_bam} --known #{dbsnp_file} \
       -R #{index_fa} \
@@ -65,7 +65,7 @@ class GatkCaller
   # Realignment
   # parallel not possible yet (1.6-13-g91f02df)
   def self.realigne(log_dir, gatk, read_bam, index_fa, target_intervals, realigned_bam, job_prefix, account, debug)
-    cmd = "qsub -o #{log_dir} -e errors_#{log_dir} -V -cwd -b y -N realignment_#{job_prefix} -l h_vmem=11G -hold_jid prep_realignment_#{job_prefix} #{account} \
+    cmd = "qsub -o #{log_dir} -e realign_errors_#{log_dir} -V -cwd -b y -N realignment_#{job_prefix} -l h_vmem=11G -hold_jid prep_realignment_#{job_prefix} #{account} \
       java -Xmx10g -jar #{gatk} \
       -I #{read_bam} \
       -R #{index_fa} \
