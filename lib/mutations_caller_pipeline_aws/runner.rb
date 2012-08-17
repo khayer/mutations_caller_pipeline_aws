3e72d3784f3740cf83799701b47c0605e0e645ce
Katharina Hayer <katharinaehayer@gmail.com>
2012-08-17 16:41:16 -0400

    added wishful thinking to runner class

--- lib/mutations_caller_pipeline_aws/runner.rb
+++ lib/mutations_caller_pipeline_aws/runner.rb
@@ -1,6 +1,6 @@
 =begin
-  * Name: SGE, part of Mutations Caller Pipeline (AWS)
-  * SGE class parses and evaluates qstat/qacct output
+  * Name: Runner, part of Mutations Caller Pipeline (AWS)
+  * Runner combines all the elements to one strong pipeline
   * Author: Katharina Hayer
   * Date: 8/17/2012
   * License: GNU General Public License (GPL-2.0)
@@ -12,10 +12,41 @@ class Runner
     @options = options
   end
 
-  def test
+  def run
     l = BwaCaller.new(@options)
     k = l.call_sampe()
     puts k
+  end
+
+  def restart
+    #restarts a run
+  end
+
+  def stop
+
+  end
+
+  def status
+
+  end
+
+
+  private
+
+  def execute(cmd)
+    ## TODO: needs to keep track of job_numbers
+    puts cmd
+    status = system(cmd)
+    raise cmd unless status
+    execute
+  end
+
+  def safe_run()
+
+  end
+
+  def load_run()
+
   end
 
 end
