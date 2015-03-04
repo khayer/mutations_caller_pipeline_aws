46b0172d59f5971404d54befe9d65b780e8cb9e8
khayer <katharinaehayer@gmail.com>
2015-03-04 18:09:11 -0500

    fixed r error

--- scripts/quick_generic.r
+++ scripts/quick_generic.r
@@ -21,7 +21,7 @@ for (k in c(1:25,"M")) {
     #abline(v = 1:13, lty = 3, col = 'purple')
     axis(side = 1, at = c(0,10000000,20000000,30000000,40000000,50000000,60000000,70000000,800000000), labels =           c("0Mbp",'10Mbp','20Mbp','30Mbp','40Mbp','50Mbp','60Mbp','70Mbp','80Mbp'))
     #title(main=paste(combo_sn2,", window_length = ",ws, "SNP, Homozygosity Score (Wik + TLF)",sep=as.character(k)),ylab="Homozygosity score")
-    title(main=sprintf("%s%i, window_length = %s SNP's, Homozygosity Score (Wik +TLF)", combo_sn2, k, ws),ylab="Homozygosity score")
+    title(main=sprintf("%s%s, window_length = %s SNP's, Homozygosity Score (Wik +TLF)", combo_sn2, k, ws),ylab="Homozygosity score")
     #sprintf("<set name=\"%s\" value=\"%i\" ></set>\n", "nina", 5)
     #legend(0,5000, c("105FEGG","125FBGB","66FEGG","71FAGA","72FAGA"), cex=0.8,
     #       col=c("red","green","blue","cyan","darkmagenta") , pch=19 )
