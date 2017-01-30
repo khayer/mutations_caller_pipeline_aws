812924bbc3a70ed1e0442bd7479b6e776a796a58
Katharina Hayer <hayerk@email.chop.edu>
2017-01-30 12:32:21 -0500

    hitting the pause button

--- lib/mutations_caller_pipeline_aws/star_caller.rb
+++ lib/mutations_caller_pipeline_aws/star_caller.rb
@@ -2,9 +2,9 @@ class StarCaller
 
   def self.call_alignment(options)
     if options[:lsf]
-      cmd = "bsub -M 30000 -n #{options[:threads]} -o #{options[:log_file]}_star_o.log -e #{options[:log_file]}_star_e.log -J star_#{options[:job_number]} #{options[:star]} --runThreadN #{options[:threads]} --genomeDir #{options[:star_index]} --readFilesIn #{options[:mutant_r1]} #{options[:mutant_r2]} --outFileNamePrefix #{options[:star_prefix]}"
+      cmd = "bsub -M 30000 -n #{options[:threads]} -o #{options[:log_file]}_star_o.log -e #{options[:log_file]}_star_e.log -J star_#{options[:job_number]} #{options[:star]} --runThreadN #{options[:threads]} --genomeDir #{options[:star_index]} --readFilesIn #{options[:mutant_r1]} #{options[:mutant_r2]} --outFileNamePrefix #{options[:star_prefix]} --outSAMstrandField intronMotif --outFilterIntronMotifs RemoveNoncanonical"
     else
-      cmd = "qsub -pe DJ #{options[:threads]} -o #{options[:log_file]}_star_o.log -e #{options[:log_file]}_star_e.log -V -cwd -b y -N star_#{options[:job_number]} -l h_vmem=6G #{options[:star]} --runThreadN #{options[:threads]} --genomeDir #{options[:star_index]} --readFilesIn #{options[:mutant_r1]} #{options[:mutant_r2]} --outFileNamePrefix #{options[:star_prefix]}"
+      cmd = "qsub -pe DJ #{options[:threads]} -o #{options[:log_file]}_star_o.log -e #{options[:log_file]}_star_e.log -V -cwd -b y -N star_#{options[:job_number]} -l h_vmem=6G #{options[:star]} --runThreadN #{options[:threads]} --genomeDir #{options[:star_index]} --readFilesIn #{options[:mutant_r1]} #{options[:mutant_r2]} --outFileNamePrefix #{options[:star_prefix]} --outSAMstrandField intronMotif --outFilterIntronMotifs RemoveNoncanonical"
     end
     cmd
   end
